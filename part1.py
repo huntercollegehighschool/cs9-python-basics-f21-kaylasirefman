f47a705636d389fe01e3ea2fe59bd3444e1b9ddc
Kayla Sirefman <94643809+kaylasirefman@users.noreply.github.com>
2021-12-05 19:29:37 +0000

    first_value = int(input("Enter a number: ")) second_value = int(input("Enter a second number: ")) third_value = int(input("Enter a third number")) fourth_value = int(input("Enter a fourth number")) fifth_value = int(input("Enter a fifth number"))
    print("The sum of the numbers you entered is", first_value + second_value + third_value + fourth_value + fifth_value)

--- part1.py
+++ part1.py
@@ -24,4 +24,4 @@ third_value = int(input("Enter a third number"))
 fourth_value = int(input("Enter a fourth number"))
 fifth_value = int(input("Enter a fifth number"))
 
-print("The sum of the numbers you entered is", first_value + second_value + third_value + fourth_value + fifth_value)
+print("The sum of the numbers you entered is", first_value + second_value + third_value + fourth_value + fifth_value) 
